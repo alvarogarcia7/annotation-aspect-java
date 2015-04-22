52d77674bdfcf3b3505c8fb22d272cc3f0788af3
Alvaro Garcia <derecerca@gmail.com>
2015-04-22 10:18:17 +0200

    refactor: extract method for readability

--- src/main/java/com/example/aspect/impl/MyAspect.java
+++ src/main/java/com/example/aspect/impl/MyAspect.java
@@ -12,7 +12,7 @@ import org.aspectj.lang.annotation.Pointcut;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 import static java.util.Arrays.*;
@@ -67,15 +67,10 @@ public class MyAspect {
 
 			final Object object = jointPoint.getTarget();
 
-			List<Field> fieldList = asList(object.getClass().getFields()).stream().collect(toList());
-			for (Field field : fieldList) {
-				for (Annotation annotation : field.getDeclaredAnnotations()) {
-					if(sameAs(annotation, InjectedLogger.class)) {
-						final Field logger = field;
-						logger.setAccessible(true);
-						((MyLogger) field.get(object)).logException(e);
-					}
-				}
+			ArrayList<MyLogger> loggers = findLoggersIn(object);
+
+			for(MyLogger logger : loggers){
+				 logger.logException(e);
 			}
 
 			final String actualExceptionName = e.getClass().getCanonicalName();
@@ -87,6 +82,21 @@ public class MyAspect {
 			}
 		}
 		return null;
+	}
+
+	private ArrayList<MyLogger> findLoggersIn(Object object) throws IllegalAccessException {
+		List<Field> fieldList = asList(object.getClass().getFields()).stream().collect(toList());
+		ArrayList<MyLogger> loggers = new ArrayList<>();
+		for (Field field : fieldList) {
+			for (Annotation annotation : field.getDeclaredAnnotations()) {
+				if(sameAs(annotation, InjectedLogger.class)) {
+					final Object logger = field.get(object);
+					field.setAccessible(true);
+					loggers.add(((MyLogger) logger));
+				}
+			}
+		}
+		return loggers;
 	}
 
 	private boolean sameAs(Annotation annotation, Class<InjectedLogger> clazz) {
