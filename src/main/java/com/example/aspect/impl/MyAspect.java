c1bdb39695461246f93b2a33dbb4d800128a98be
Alvaro Garcia <derecerca@gmail.com>
2015-04-22 10:23:04 +0200

    refactor: reorder parameters so it is read like English:
    logInAll(loggers. What? the exception

--- src/main/java/com/example/aspect/impl/MyAspect.java
+++ src/main/java/com/example/aspect/impl/MyAspect.java
@@ -63,7 +63,7 @@ public class MyAspect {
 			return proceed;
 		} catch (Exception e){
 			ArrayList<MyLogger> loggers = findLoggersIn(jointPoint);
-			logInAll(e, loggers);
+			logInAll(loggers, e);
 			captureOrRethrow(exceptionNames, e);
 		}
 		return null;
@@ -85,7 +85,7 @@ public class MyAspect {
 		}
 	}
 
-	private void logInAll(Exception e, ArrayList<MyLogger> loggers) {
+	private void logInAll(ArrayList<MyLogger> loggers, Exception e) {
 		for(MyLogger logger : loggers){
 			 logger.logException(e);
 		}
